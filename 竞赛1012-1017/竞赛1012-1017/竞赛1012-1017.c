acb99293cd534e50090dda29183c716db0864459
zhangsenyu <ruanyu66510@163.com>
2022-10-22 17:34:52 +0800

    竞赛

--- 竞赛1012-1017/竞赛1012-1017/竞赛1012-1017.c
+++ 竞赛1012-1017/竞赛1012-1017/竞赛1012-1017.c
@@ -1,9 +1,20 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<string.h>
+#include<math.h>
 #include<time.h>
-//1012
-//输入一行字符，分别统计出其中英文字母、数字、空格和其他字符的个数。
+//打着玩
+//int main()
+//{
+//	printf("**************************\n");
+//	printf("Hello World!\n");
+//	printf("**************************\n");
+//	return 0;
+//}
+
+////1012
+//// 输入一行字符，分别统计出其中英文字母、数字、空格和其他字符的个数。
+//// 
 //int main()
 //{
 //    char str[100];
@@ -23,44 +34,23 @@
 //    printf("%d %d %d %d", letter, number, space, other);
 //    return 0;
 //}
-// //巧妙利用ausm值
-//小测验
-
-//int main()
-//{
-//    char arr[100] = { 0 };
-//    scanf("%s", arr);
-//    for (int i = 0; i <= 9; i++)
-//    {
-//        printf("%d ", arr[i]);
-//    }
-//    return 0;
-//}
-
- 
 
 //1013
-//求Sn = a + aa + aaa + … + aa…aaa（有n个a）之值，其中a是一个数字，为2。 
-//例如，n = 5时 = 2 + 22 + 222 + 2222 + 22222，n由键盘输入。
-
+//求Sn=a+aa+aaa+…+aa…aaa（有n个a）之值，
+//其中a是一个数字，为2。 例如，n=5时=2+22+222+2222+22222，n由键盘输入。
 //int main()
-//
 //{
-//	int a, b = 2, c = 1,d = 10,e = 0;
-//	scanf("%d", &a);
-//	for ( c = 1; c <= a; c++ )
+//	int a, i, c = 2, d = 2;
+//	scanf("%d", &i);
+//	for (a = 1; a < i; a++)
 //	{
-//		b = (b % 10) * d + b;
-//		
-//		d = d * 10;
-//
-//		e = e + b;
+//		c = c * 10 + 2;
+//		d = d + c;
 //	}
-//	printf("%d", e);
-//
+//	printf("%d", d);
 //	return 0;
 //}
-
+//
 //int main()
 //{
 //	int n, Sn = 0, i, a = 0;
@@ -73,59 +63,3 @@
 //	return 0;
 //}
 
-
-
-
-//1014
-//求Sn = 1!+ 2!+ 3!+ 4!+ 5!+ … + n!之值，其中n是一个数字(n不超过20)。
- 
-
-int main()
-{
-	int Sn, i, a, n;
-	scanf("%d", &a);
-	for (i = 0; i <= a; i++)
-	{
-		n=i
-
-
-	}
-	
-	
-	
-	return 0;
-}
-
-
-
-
-//1015
-
-
-//int main()
-//{
-//
-//	return 0;
-//}
-
-
-
-//1016
- 
-
-//int main()
-//{
-//
-//	return 0;
-//}
-
-
-
-//1017
-
-
-//int main()
-//{
-//
-//	return 0;
-//}
